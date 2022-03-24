150c004f42c17bcc970aa5f7858aef3d38dcdc00
mgilberg <mgilberg25@penguinhall.org>
2022-03-24 09:30:34 -0400

    second edit

---
+++ refresher.js
@@ -0,0 +1,59 @@
+// console.log("Hello world!");
+
+// var message = "How are you today?";
+// // console.log(message);
+
+// var greeting = "Hello";
+// var name = "Sue";
+// // console.log(greeting + " " + name + "!");
+
+// let age = 13;
+// console.log("You are " + age);
+
+// console.log("In ten years you will be " + (age+10));
+
+// This is the start of part 2
+
+// if (name == "Bob") {
+//     console.log("I knew it was you, "+name+"!");
+// } else {
+//     console.log("Hey "+name+", you're not Bob!");
+// }
+
+// var iAmWearingABelt = true;
+// var iAmWearingGlasses = false;
+
+// if (iAmWearingABelt == true && iAmWearingGlasses == true) {
+//     console.log("Probably not Mr. Gorton");
+// } else if(iAmWearingABelt == true && iAmWearingGlasses == false) {
+//     console.log("Probably Mr. Gorton");
+// } else {
+//     console.log("Probably just some dude");
+// }
+
+// Start of part 3
+
+// for (let i = 0; i < 6; i++) { 
+//     console.log("comp sci is awesome");
+// }
+
+// let num = 0;
+// for (let i = 0; i < 10; i++) { 
+//     num++;
+//     console.log(num);
+// }
+
+// tellMeMyAge();
+// function tellMeMyAge() {
+//     var age = 13;
+//     console.log("You are " + age + " years old.");
+
+// }
+makePizza("pepperoni", "small");
+makePizza("broccoli", "large");
+
+function makePizza(type,size) {
+    console.log("One " + size + " " + type + " pizza coming up!");
+}
+
+
