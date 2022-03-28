e9564b449b9deee7c53e27f05e8f8a9781b543f1
mgilberg <mgilberg25@penguinhall.org>
2022-03-28 13:31:39 -0400

    edit 1

---
+++ DeckOfCards/fizz-buzz.js
@@ -0,0 +1,11 @@
+for (var i = 1; i<50; i++) {
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log(i + " fizz buzz");
+    } else if (i % 3 === 0) {
+        console.log(i + " fizz");
+    } else if (i % 5 === 0) {
+        console.log(i + " buzz")
+    } else {
+        console.log(i);
+    }
+}
