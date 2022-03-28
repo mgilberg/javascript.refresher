df4783ef84e93030b4239e0967f0ab60916f87a9
mgilberg <mgilberg25@penguinhall.org>
2022-03-28 13:09:24 -0400

    edit 2 (with i)

--- DeckOfCards/deck-of-cards.js
+++ DeckOfCards/deck-of-cards.js
@@ -1,17 +1,12 @@
-var numOfHearts = 1;
-var numOfDiamonds = 1;
-var numOfSpades = 1;
-var numOfClubs = 1;
-writeCards(numOfHearts,"Hearts");
-writeCards(numOfDiamonds,"Diamonds");
-writeCards(numOfSpades,"Spades");
-writeCards(numOfClubs,"Clubs");
-function writeCards(num,suit) {
+writeCards("Hearts");
+writeCards("Diamonds");
+writeCards("Spades");
+writeCards("Clubs");
+function writeCards(suit) {
     console.log(" ");
     console.log("Ace of " + suit);
-    for (var i = 0; i<9; i++) {
-        num++;
-        console.log(num + " of " + suit);
+    for (var i = 2; i<11; i++) {
+        console.log(i + " of " + suit);
     }
     console.log("Jack of " + suit);
     console.log("Queen of " + suit);
