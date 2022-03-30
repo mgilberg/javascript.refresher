77165ac8ff90f5d4827db50b4a4dd536e12053bb
mgilberg <mgilberg25@penguinhall.org>
2022-03-30 10:12:53 -0400

    edit 5

--- DeckOfCards/fizz-buzz.js
+++ DeckOfCards/fizz-buzz.js
@@ -1,8 +1,8 @@
 var fizz = 0;
 var buzz = 0;
 var fizzBuzz = 0;
-for (var i = 1; i<1000; i++) {
-    if (i % 3 === 0 && i % 5 === 0){
+for (var i = 1; i < 1001; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log(i + " fizz buzz");
         fizzBuzz++;
     } else if (i % 3 === 0) {
@@ -15,4 +15,4 @@ for (var i = 1; i<1000; i++) {
         console.log(i);
     }
 }
-console.log("There were "+fizz+" fizzes, "+buzz+" buzzes, and "+fizzBuzz+" fizz buzzes out of 1000.");
+console.log("There were " + fizz + " fizzes, " + buzz + " buzzes, and " + fizzBuzz + " fizz buzzes out of 1000.");
