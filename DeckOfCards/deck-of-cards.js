56747c7c6e9286b3eb207720a08ef19a9870f482
mgilberg <mgilberg25@penguinhall.org>
2022-03-29 21:25:54 -0400

    edit 4

--- DeckOfCards/deck-of-cards.js
+++ DeckOfCards/deck-of-cards.js
@@ -2,10 +2,10 @@ writeCards("Hearts");
 writeCards("Diamonds");
 writeCards("Spades");
 writeCards("Clubs");
+
 function writeCards(suit) {
-    console.log(" ");
-    console.log("Ace of " + suit);
-    for (var i = 2; i<11; i++) {
+    console.log("\nAce of " + suit);
+    for (var i = 2; i < 11; i++) {
         console.log(i + " of " + suit);
     }
     console.log("Jack of " + suit);
