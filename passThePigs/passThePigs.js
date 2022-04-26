d61e0877ea532e9012994b23d5080f5f9a27a4ec
mgilberg <mgilberg25@penguinhall.org>
2022-04-26 15:18:20 -0400

    new edit

--- passThePigs/passThePigs.js
+++ passThePigs/passThePigs.js
@@ -1,6 +1,5 @@
 let playerTurn = 0;
-let pick1;
-let pick2;
+let pick;
 let score = 0;
 let pig1;
 let pig2;
@@ -10,14 +9,12 @@ let totalScore2 = 0;
 let totalScore3 = 0;
 let addingScore;
 
-
-// disables pass button for start
+// disables buttons for start
 document.getElementById("player0PassButton").disabled = false;
 document.getElementById("player1PassButton").disabled = true;
 document.getElementById("player2PassButton").disabled = true;
 document.getElementById("player3PassButton").disabled = true;
 
-// disables roll button for start 
 document.getElementById("player0RollButton").disabled = false;
 document.getElementById("player1RollButton").disabled = true;
 document.getElementById("player2RollButton").disabled = true;
@@ -56,7 +53,6 @@ function handleReplay() {
     document.getElementById("replay").className = "w3-row w3-container w3-hide";
 }
 function handlePass() {
-    
     if (playerTurn == 0) {
         pass(0, 1, true, false, true, true);
     } else if (playerTurn == 1) {
@@ -68,7 +64,7 @@ function handlePass() {
     }
 
     playerTurn = playerTurn + 1;
-    if (playerTurn === 4) {
+    if (playerTurn >= 4) {
         playerTurn = 0;
     }
 }
@@ -94,7 +90,7 @@ function pass(num, num2, a, b, c, d) {
         document.getElementById("player" + 0 + "TotalScore").textContent = "Total Score: " + totalScore0;
     } else if (playerTurn === 1) {
         totalScore1 = totalScore1 + score;
-        document.getElementById("player" + 1 + "TotalScore").textContent = "Total Score: " + totalScore2;
+        document.getElementById("player" + 1 + "TotalScore").textContent = "Total Score: " + totalScore1;
     } else if (playerTurn === 2) {
         totalScore2 = totalScore2 + score;
         document.getElementById("player" + 2 + "TotalScore").textContent = "Total Score: " + totalScore2;
@@ -102,7 +98,7 @@ function pass(num, num2, a, b, c, d) {
         totalScore3 = totalScore3 + score;
         document.getElementById("player" + 3 + "TotalScore").textContent = "Total Score: " + totalScore3;
     }
-
+    document.getElementById("player" + playerTurn + "HandScore").textContent = "Score: 0";
     score = 0;
 }
 function scoreRoll(pig1, pig2) {
@@ -163,30 +159,11 @@ function findPig(i) {
         return "Leaning Jowler";
     }
 }
-function handleRoll() {
-    pig1 = findPig(1);
-    pig2 = findPig(2);
-
-    // update screen
-    document.getElementById("player" + playerTurn + "Pig1").textContent = pig1;
-    document.getElementById("player" + playerTurn + "Pig2").textContent = pig2;
-
-    let rollScore = scoreRoll(pig1, pig2);
-
-    if (rollScore === 0) {
-        score = 0;
-        pigOut();
-
-    } else {
-        score = score + rollScore;
-        document.getElementById("player" + playerTurn + "HandScore").textContent = "Score: " + score;
-    }
-}
 function scoreRoll(pig1, pig2) {
     addingScore = 0;
     if ((pig1 == "Dot" && pig2 == "No Dot") || (pig2 == "Dot" && pig1 == "No Dot")) {
         return 0;
-    } else if ((pig1 == "Dot" && pig2 == "Dot") || (pig1 == "No Dot" && pig2 == "No Dot") || pig1 == "Dot" || pig2 == "Dot" || pig1 == "No Dot" || pig2 == "No Dot") {
+    } else if ((pig1 == "Dot" && pig2 == "Dot") || (pig1 == "No Dot" && pig2 == "No Dot")) {
         addingScore = addingScore + 1;
     }
     if ((pig1 == "Razorback" && pig2 == "Razorback") || (pig1 == "Trotter" && pig2 == "Trotter")) {
@@ -208,43 +185,38 @@ function scoreRoll(pig1, pig2) {
 }
 function pigOut() {
     score = 0;
-    document.getElementById("player" + playerTurn + "HandScore").textContent = "PIG OUT!";
-
     if (playerTurn === 0) {
         pass(0, 1, true, false, true, true);
     } else if (playerTurn === 1) {
         pass(1, 2, true, true, false, true);
-        score = 0;
     } else if (playerTurn === 2) {
         pass(2, 3, true, true, true, false);
-        score = 0;
     } else {
         pass(3, 0, false, true, true, true);
     }
     playerTurn++;
 }
-function findPig(i) {
-    picki = Math.floor(Math.random() * 1000) + 1;
+function findPig() {
+    pick = Math.floor(Math.random() * 1000) + 1;
 
-    if (picki <= 349) {
+    if (pick <= 349) {
         return "Dot";
-    } else if (picki <= 651 && picki >= 350) {
+    } else if (pick <= 651 && pick >= 350) {
         return "No Dot";
-    } else if (picki >= 652 && picki <= 875) {
+    } else if (pick >= 652 && pick <= 875) {
         return "Razorback";
-    } else if (picki >= 876 && picki <= 963) {
+    } else if (pick >= 876 && pick <= 963) {
         return "Trotter";
-    } else if (picki >= 964 && picki <= 993) {
+    } else if (pick >= 964 && pick <= 993) {
         return "Snouter";
     } else {
         return "Leaning Jowler";
     }
 }
 function handleRoll() {
-    pig1 = findPig(1);
-    pig2 = findPig(2);
+    pig1 = findPig();
+    pig2 = findPig();
 
-    // update screen
     document.getElementById("player" + playerTurn + "Pig1").textContent = pig1;
     document.getElementById("player" + playerTurn + "Pig2").textContent = pig2;
 
@@ -253,7 +225,7 @@ function handleRoll() {
     if (rollScore === 0) {
         score = 0;
         pigOut();
-
+        document.getElementById("player" + (playerTurn-1) + "HandScore").textContent = "PIG OUT!";
     } else {
         score = score + rollScore;
         document.getElementById("player" + playerTurn + "HandScore").textContent = "Score: " + score;
@@ -278,6 +250,9 @@ function reset() {
     document.getElementById("player3RollButton").disabled = true;
 }
 function handleClick(id) {
+    if (playerTurn >= 4) {
+        playerTurn = 0;
+    }
 
     if (id == "player0PassButton" || id == "player1PassButton" || id == "player2PassButton" || id == "player3PassButton") {
         handlePass();
@@ -286,24 +261,19 @@ function handleClick(id) {
     } else {
         handleReplay();
     }
-    
 }
 function checkWinner() {
-    if (totalScore0 + score >= 10) {
+    if ((totalScore0 + score >= 50) && playerTurn == 0) {
         reset();
-        console.log("1");
         document.getElementById("player0").className = "w3-card w3-container w3-yellow w3-round-large";
-    } else if (totalScore1 + score >= 10) {
+    } else if ((totalScore1 + score >= 50) && playerTurn == 1) {
         reset();
-        console.log("2");
         document.getElementById("player1").className = "w3-card w3-container w3-yellow w3-round-large";
-    } else if (totalScore2 + score >= 10) {
+    } else if ((totalScore2 + score >= 50) && playerTurn == 2) {
         reset();
-        console.log("3");
         document.getElementById("player2").className = "w3-card w3-container w3-yellow w3-round-large";
-    } else if (totalScore3 + score >= 10) {
+    } else if ((totalScore3 + score >= 50) && playerTurn == 3) {
         reset();
-        console.log("4");
         document.getElementById("player3").className = "w3-card w3-container w3-yellow w3-round-large";
-    }
+    } 
 }
